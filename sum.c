3ef6dde8c769adc0b034d96bb24d61ec8c4d3037
pavani582-0705 <pavanirani2005@gmail.com>
2025-11-27 10:46:24 +0530

    addition of two nums

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    printf("Enter a value:");
+    scanf("%d",&a);
+    printf("Enter b value:");
+    scanf("%d",&b);
+    c = a+b;
+    printf("sum %d and %d =%d",a,b,c);
+return 0;
+}
