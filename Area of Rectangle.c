d4262853fccde0eb1fb89b4d39a7415f5a5bf3ff
pavani582-0705 <pavanirani2005@gmail.com>
2025-11-27 20:24:48 +0530

    Area of Rectangle

---
+++ Area of Rectangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+  int length,breadth;
+  printf("Enter length of a Rectangle:");
+  scanf("%d",&length);
+  printf("Enter breadth of a Rectangle:");
+  scanf("%d",&breadth);
+  printf("Area of a Rectangle:%d",length*breadth);
+   return 0;
+}
