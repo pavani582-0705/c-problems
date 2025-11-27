9eda59b6708b6bdc3a2b0a4f31fe41774a31ecf5
pavani582-0705 <pavanirani2005@gmail.com>
2025-11-27 18:21:15 +0530

    largest of three numbers

---
+++ largest of three numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+{
+   int num1,num2,num3;
+   printf("Enter num1,num2,num3:");
+   scanf("%d%d%d",&num1,&num2,&num3);
+   if(num1>num2 && num1>num3)
+   {
+       printf("%d is a largest number");
+   }
+   else if(num2>num1 && num2>num3)
+   {
+       printf("%d is a largest number");
+   }
+   else
+   {
+       printf("%d is a largest number");
+   }
+
+    return 0;
+}
