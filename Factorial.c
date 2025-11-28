2ff8e33e3a4dfe66c02c5f9aec6df868791a3527
pavani582-0705 <pavanirani2005@gmail.com>
2025-11-28 07:41:13 +0530

    Factorial of given number

---
+++ Factorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    int n,i,fact=1;
+    printf("Enter n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+        printf("Factorial of a %d is : %d",n,fact);
+    return 0;
+}
