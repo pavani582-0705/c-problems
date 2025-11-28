2fde4d9b2bdae06014806280883c056fdb3ad3da
pavani582-0705 <pavanirani2005@gmail.com>
2025-11-28 08:28:29 +0530

    factorial using recursion

---
+++ factorial using recursion.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int fact(int);
+int main()
+{
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    if(n<0)
+    {
+        printf("Factorial is not defined for Negative numbers");
+    }
+    else
+    {
+    printf("Fcatorial of %d is: %d",n,fact(n));
+    }
+    return 0;
+}
+int fact(int n)
+{
+    if(n == 0|| n ==1)
+    {
+       return 1;
+    }
+    else {
+        return n*fact(n-1);
+        
+    }
+}
